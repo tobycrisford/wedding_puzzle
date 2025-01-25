8624090a1cd7659595266b7fded4af725f929076
Toby Crisford <tobycrisford@gmail.com>
2025-01-25 09:40:51 +0000

    Creating puzzle password entry page

--- puzzle.js
+++ puzzle.js
@@ -1,6 +1,10 @@
+async function load_config() {
+    return await fetch('crypto_config.json').then((response) => response.json());
+}
+
 async function generate_key(pswd) {
     
-    crypto_config = await fetch('crypto_config.json').then((response) => response.json());
+    crypto_config = await load_config();
     
     const encoder = new TextEncoder();
     const data = encoder.encode(pswd);
@@ -23,7 +27,7 @@ async function generate_key(pswd) {
         keyMaterial,
         { name: 'AES-GCM', length: 256 },
         true,
-        ['encrypt']
+        ['encrypt', 'decrypt']
     );
 }
 
@@ -46,9 +50,49 @@ async function encrypt_text(plaintext, pswd) {
     return btoa(String.fromCharCode.apply(null, new Uint8Array(encryptedData)));;
 }
 
+async function decrypt_text(cyphertext, pswd) {
+
+    const key = await generate_key(pswd);
+
+    const encryptedData = Uint8Array.from(atob(cyphertext), c => c.charCodeAt(0)).buffer;
+
+    const decryptedData = await window.crypto.subtle.decrypt(
+        {
+            name: 'AES-GCM',
+            iv: new Uint8Array(12) // Not important as this is a one-time puzzle
+        },
+        key,
+        encryptedData
+    );
+
+    const decoder = new TextDecoder();
+    const plaintext = decoder.decode(decryptedData);
+
+    return plaintext;
+}
+
 async function encrypt() {
     const plaintext = document.getElementById('user_msg').value;
     const pswd = document.getElementById('pswd').value;
 
     document.getElementById('output').innerText = await encrypt_text(plaintext, pswd);
+}
+
+async function decrypt() {
+    const pswd = document.getElementById('pswd').value;
+    const cyphertext = await fetch('encrypted_text.txt').then((response) => response.text());
+
+    try {
+        document.getElementById('output').innerText = await decrypt_text(cyphertext, pswd);
+    }
+    catch (e) {
+        if (e.name === 'OperationError') {
+            document.getElementById('output').innerText = 'Incorrect password';
+        }
+        else {
+            document.getElementById('output').innerText = 'An error occurred';
+        }
+    }
+
+    
 }
